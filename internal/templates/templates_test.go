7a44615f5b8a638d2f2fc4b632827d49d5dfc98b
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-10-23 20:21:09 +0200

    test: function for formatting duration

---
+++ internal/templates/templates_test.go
@@ -0,0 +1,28 @@
+package templates
+
+import (
+	"testing"
+)
+
+func TestFormatSecs(t *testing.T) {
+	tests := []struct{
+		input uint32
+		expectedOutput string
+	}{
+		{0, "0s"},
+		{5, "5s"},
+		{59, "59s"},
+		{60, "1m"},
+		{75, "1m 15s"},
+		{3599, "59m 59s"},
+		{3600, "1h"},
+		{3723, "1h 2m 3s"},
+		{7265, "2h 1m 5s"},
+	}
+
+	for _, tt := range tests {
+		if got, want := formatSecs(tt.input), tt.expectedOutput; got != want {
+			t.Errorf("input=%v, got=%v, want=%v", tt.input, got, want)
+		}
+	}
+}
